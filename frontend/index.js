4659c82076189d197c040508f7b7e6d3a0cb1d2a
Alex Monte <alexmonte@tre-al.jus.br>
2021-11-14 13:09:11 -0300

    Passphrase password generation implemented

--- frontend/index.js
+++ frontend/index.js
@@ -1,5 +1,5 @@
 async function loadWordlist(locale) {
-    let selectedLocale = locale | 'pt-BR.json';
+    let selectedLocale = locale || 'pt-BR';
     let wordlistUri = `wordlist/${selectedLocale}.json`;
     let response = await fetch(wordlistUri);
 
@@ -7,17 +7,75 @@ async function loadWordlist(locale) {
 }
 
 function rollDices(nDices) {
-    let results = Uint16Array(nDices);
+    let dices = new Uint16Array(nDices);
+    let result = '';
 
-    crypto.getRandomValues(results);
+    crypto.getRandomValues(dices);
     for (let i = 0; i < nDices; i++) {
-        results[i] = results[i] % 7777;
+        result = result.concat(dices[i] % 6);
     }
-    return results;
+    return result;
 }
 
 function pickWords(nWords, wordList) {
-    let wordIndexes = rollDices(nWords);
-    
-    return wordIndexes.map((v, i, a) => wordList[v]);
+    const words = [];
+    const nDices = findNumberOfDicesForNumber(wordList.length);
+
+    for (let i = 0; i < nWords; i++) {
+        let dices = rollDices(nDices);
+        let wordIndex = dicesToInt(dices, wordList.length);
+
+        words.push(wordList[wordIndex]);
+    }
+    return words;
+}
+
+function findNumberOfDicesForNumber(n) {
+    const base = 6;
+
+    return parseInt(Math.floor(Math.log(n) / Math.log(base)) + 1);
+}
+
+function dicesToInt(dices, maxValue) {
+    return parseInt(dices, 6) % maxValue;
+}
+
+function pickRandomSymbol() {
+    const symbols = ['-', '/', ':', '$', '&', '@', '.', ',', '?', '!'];
+    const nDices = findNumberOfDicesForNumber(symbols.length);
+    const dices = rollDices(nDices);
+    const index  = dicesToInt(dices, symbols.length);
+
+    return symbols[index];
+}
+
+function startCase(word) {
+    let [first, ...rest] = word;
+
+    return first.toUpperCase() + rest.join('');
+}
+
+function generateNumber(maxValue) {
+    const nDices = findNumberOfDicesForNumber(maxValue);
+    const dices = rollDices(nDices);
+
+    return dicesToInt(dices, maxValue);
+}
+
+function generatePassPhrase(nWords, wordList, hasSpecialSymbos, hasStartCase, hasNumber, maxPassPhraseLength = 64) {
+    let words = pickWords(nWords, wordList);
+    const symbol = hasSpecialSymbos
+            ? pickRandomSymbol()
+            : ' ';
+
+    if (hasStartCase) {
+        words = words.map((v, i, a) => startCase(v));
+    }
+    if (hasNumber) {
+        words = words.map((v, i, a) => i % 2 > 0 ? generateNumber(1000) : v);
+    }
+    while (words.join(' ').length > maxPassPhraseLength) {
+        words.pop();
+    }
+    return words.join(symbol);   
 }
